4fee86924a3ab221f7ec8d71a17caa8329d840b3
Grabenkind <stevenmueller99@outlook.de>
2021-05-10 16:34:11 +0200

    added copy button and function

--- src/de/grabenkind/uuidgenerator/App.java
+++ src/de/grabenkind/uuidgenerator/App.java
@@ -2,6 +2,8 @@ package de.grabenkind.uuidgenerator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.UUID;
@@ -10,21 +12,33 @@ public class App {
 	private JButton button1;
 	private JPanel panel1;
 	private JTextPane textPane1;
+	private JButton copyButton;
+
+	String uuid;
 
 	public App() {
-
 		textPane1.setContentType("text/html");
 		textPane1.setEditable(false);
 		textPane1.setBackground(null);
 		textPane1.setBorder(null);
 		textPane1.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
 		textPane1.setFont(new Font("", Font.BOLD, 18));
-		textPane1.setText(UUID.randomUUID().toString());
+		uuid = UUID.randomUUID().toString();
+		textPane1.setText(uuid);
 
 		button1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				textPane1.setText(UUID.randomUUID().toString());
+				uuid = UUID.randomUUID().toString();
+				textPane1.setText(uuid);
+			}
+		});
+		copyButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				StringSelection stringSelection = new StringSelection(uuid);
+				Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+				clipboard.setContents(stringSelection, null);
 			}
 		});
 	}
